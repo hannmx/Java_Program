7fbfdbfa6499dbb201a72d12e7a0a02b054bba13
hannmx <mxkud@mail.ru>
2023-04-04 22:18:07 +0300

    Создан класс Категория в java_Product

--- j_oop/java_Product/Category.java
+++ j_oop/java_Product/Category.java
@@ -1,5 +1,31 @@
 package j_oop.java_Product;
 
 public class Category {
-    
+    String categoryName;
+    Product [] products = new Product[3];
+
+    public Category(String categoryName, Product[] products) {
+        this.categoryName = categoryName;
+        this.products = products;
+    }
+
+    public Category() {
+
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Product[] getProduct() {
+        return products;
+    }
+
+    public void setProduct(Product[] products) {
+        this.products = products;
+    }
 }
