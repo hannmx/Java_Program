e3b568bc846843c92016e4e95a131182e9801295
hannmx <mxkud@mail.ru>
2023-04-04 22:23:49 +0300

    Создан класс Basket в java_Product

--- j_oop/java_Product/Basket.java
+++ j_oop/java_Product/Basket.java
@@ -1,5 +1,21 @@
 package j_oop.java_Product;
 
 public class Basket {
-    
+    Product [] buyProducts = new Product[3];
+
+    public Basket(Product[] buyProducts) {
+        this.buyProducts = buyProducts;
+    }
+
+    public Basket() {
+        
+    }
+
+    public Product[] getBuyProducts() {
+        return buyProducts;
+    }
+
+    public void setBuyProducts(Product[] buyProducts) {
+        this.buyProducts = buyProducts;
+    }
 }
