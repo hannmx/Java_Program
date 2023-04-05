9de007119bee4f7388385a5ce5a9e17e001877a9
hannmx <mxkud@mail.ru>
2023-04-05 21:14:32 +0300

    Реализована программа по прохождению атлетами полосы препятствий

--- j_oop/java_Man_Cat_Robot/Main.java
+++ j_oop/java_Man_Cat_Robot/Main.java
@@ -1,7 +1,34 @@
 package j_oop.java_Man_Cat_Robot;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         
+        Random random = new Random();
+        
+        Athletics[] athletes = {
+            new Human(random.nextInt(500,3001), random.nextInt(100,301)),
+            new Cat(random.nextInt(100,1501), random.nextInt(100,201)),
+            new Human(random.nextInt(500,3001), random.nextInt(0,301)),
+            new Robot(random.nextInt(500,3001), random.nextInt(0,301)),
+            new Cat(random.nextInt(100,1501), random.nextInt(0,201)),
+            new Human(random.nextInt(500,3001), random.nextInt(0,301)),
+            new Robot(random.nextInt(500,3001), random.nextInt(100,301))
+        };
+
+        Cross[] elements = {
+            new RunTrack(200),
+            new Barrier(50),
+            new Barrier(30),
+            new RunTrack(1500),
+            new Barrier(100)
+        };
+
+        for (Athletics athlete : athletes) {
+            for (Cross equipment: elements) {
+                equipment.cross(athlete);
+            }
+        }
     }
 }
