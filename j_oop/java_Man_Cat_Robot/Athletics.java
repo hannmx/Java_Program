046fa42169045aaa170c8cf673674d3e061452dd
hannmx <mxkud@mail.ru>
2023-04-05 18:44:32 +0300

    Создан класс Human и интерфейс Athletics

---
+++ j_oop/java_Man_Cat_Robot/Athletics.java
@@ -0,0 +1,6 @@
+package j_oop.java_Man_Cat_Robot;
+
+public interface Athletics {
+    void run(int length);
+    void jump(int height);
+}
---
+++ j_oop/java_Man_Cat_Robot/Main.java
@@ -0,0 +1,7 @@
+package j_oop.java_Man_Cat_Robot;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
