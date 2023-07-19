60ec7df898d9de7202fe376dbf07d78320360627
hannmx <mxkud@mail.ru>
2023-07-19 17:56:50 +0300

    Метод разворота связного списка на Java

---
+++ java_projects/j_listnode.java
@@ -0,0 +1,50 @@
+package java_projects;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+public class j_listnode {
+    public static ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev; // новый головной узел
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        System.out.println("Исходный список:");
+        printLinkedList(head);
+
+        head = reverseLinkedList(head);
+
+        System.out.println("Список после разворота:");
+        printLinkedList(head);
+    }
+
+    public static void printLinkedList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
