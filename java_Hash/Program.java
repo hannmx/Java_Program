c777cba44b944c3552e9f8d3e1892605a0d4fec8
hannmx <mxkud@mail.ru>
2023-07-23 18:49:24 +0300

    Структура HashMap с семинара

---
+++ java_Hash/Program.java
@@ -0,0 +1,37 @@
+package java_Hash;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee("AAA", 30);
+        System.out.println(employee1.hashCode());
+
+        j_HashMap<String, String> hashMap = new j_HashMap<>(4);
+
+
+        String v = hashMap.put("+79005551122", "Александр");
+        v = hashMap.put("+79005551123", "Сергей");
+        v = hashMap.put("+79005551123", "Алексей");
+        v = hashMap.put("+79005551124", "Александр1");
+        v = hashMap.put("+79005551125", "Александр2");
+        v = hashMap.put("+79005551126", "Александр3");
+        v = hashMap.put("+79005551127", "Александр4");
+        v = hashMap.put("+79005551128", "Александр5");
+
+
+        String searchRes = hashMap.get("+790055511221");
+
+        v = hashMap.remove("+79005551127");
+
+        for (Object e: hashMap) {
+
+        }
+
+    }
+
+}
+
